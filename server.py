bbba4765379bd80f846acf1778ff74724c384af0
gaeun5142-afk <gaeun5142@gmail.com>
2025-11-23 10:39:08 +0900

    Update server.py

--- server.py
+++ server.py
@@ -463,7 +463,7 @@ def create_standard_excel(data):
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return "Food Label API is running", 200
 
 
 # 1단계: 정답지 만들기 (엑셀 + 원재료 사진들 몽땅)
